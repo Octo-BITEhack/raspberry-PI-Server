c732c64bfd944625071eb14057f8fc1d1753ba54
kn2282 <57344058+kn2282@users.noreply.github.com>
2023-01-15 10:13:39 +0100

    up

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ PUMP_GPIO = 17
 FAN_GPIO = 18
 MAIN_PERIOD = 1
 
+
+
+
+pump_controller = gpio_controler.GPIOController(PUMP_GPIO)
+fan_controller = gpio_controler.GPIOController(FAN_GPIO)
+
 # global variables
 global PULSE
 global SATURATION
@@ -54,22 +60,33 @@ def stats():
     'isFunOn': IS_FUN_ON
   }
 
-@app.route('/helmet', methods=['POST'])
-def helmet():
-  global IS_HELMET_OPEN
-  IS_HELMET_OPEN = request.json['isHelmetOpen']
-  return str(IS_HELMET_OPEN)
+# @app.route('/helmet', methods=['POST'])
+# def helmet():
+#   global IS_HELMET_OPEN
+#   if request.json['isHelmetOpen']:
+#     fan_controller.turn_on()
+#   else:
+#     fan_controller.turn_off()
+#   return str(IS_HELMET_OPEN)
 
 @app.route('/fan', methods=['POST'])
 def fan():
   global IS_FUN_ON
   IS_FUN_ON = request.json['isFanOn']
+  if request.json['isFanOn']:
+    fan_controller.turn_on()
+  else:
+    fan_controller.turn_off()
   return str(IS_FUN_ON)
 
 @app.route('/beer', methods=['POST'])
 def beer():
   global IS_BEER_BEING_DRANK
   IS_BEER_BEING_DRANK = request.json['isBeerBeingDrank']
+  if request.json['isBeerBeingDrank']:
+    fan_controller.turn_on()
+  else:
+    fan_controller.turn_off()
   return str(IS_BEER_BEING_DRANK)
 
 def main_loop(puls_oximetr, light_sensor, sound_sensor, pump_controller, fan_controller):
@@ -86,16 +103,16 @@ def main_loop(puls_oximetr, light_sensor, sound_sensor, pump_controller, fan_con
         SATURATION = puls_oximetr.get_saturation()
         IS_LIGHT = light_sensor.isLow()
         IS_NOISE = sound_sensor.isLow()
-        # IS_HELMET_OPEN = False
-        if  IS_BEER_BEING_DRANK and pump_controller.is_off:
-            pump_controller.turn_on()
-        if (not IS_BEER_BEING_DRANK) and pump_controller.is_on:
-            pump_controller.turn_off()
+        # # IS_HELMET_OPEN = False
+        # if  IS_BEER_BEING_DRANK and pump_controller.is_off:
+        #     pump_controller.turn_on()
+        # if (not IS_BEER_BEING_DRANK) and pump_controller.is_on:
+        #     pump_controller.turn_off()
         
-        if  IS_FUN_ON and pump_controller.is_off:
-            fan_controller.turn_on()
-        if (not IS_FUN_ON) and pump_controller.is_on:
-            fan_controller.turn_off()
+        # if  IS_FUN_ON and pump_controller.is_off:
+        #     fan_controller.turn_on()
+        # if (not IS_FUN_ON) and pump_controller.is_on:
+        #     fan_controller.turn_off()
 
         print({
             'pulse': PULSE,
@@ -111,8 +128,8 @@ def main():
     puls_oximetr = puls_meter.PulsMeter()
     light_sensor = gpio_inputs.GPIO_input(LIGHT_GPIO)
     sound_sensor = gpio_inputs.GPIO_input(SOUND_GPIO)
-    pump_controller = gpio_controler.GPIOController(PUMP_GPIO)
-    fan_controller = gpio_controler.GPIOController(FAN_GPIO)
+    # pump_controller = gpio_controler.GPIOController(PUMP_GPIO)
+    # fan_controller = gpio_controler.GPIOController(FAN_GPIO)
     t = threading.Thread(target=main_loop, args=(puls_oximetr, light_sensor, sound_sensor, pump_controller, fan_controller))
     t.start()
 
